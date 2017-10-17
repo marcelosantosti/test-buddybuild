38140106fc0015dab5ea86545e303ce831e3b970
Marcelo Luis dos Santos <marcelo@connectedinteractive.com>
2017-10-17 13:21:04 -0200

    Adding library with instrumentation tests.

---
+++ test-buddybuild-library/src/androidTest/java/marcelosantosti/github/io/test_buddybuild_library/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package marcelosantosti.github.io.test_buddybuild_library;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void getPlatform_shouldNotBeEmpty() {
+        String platform = OsInfo.getPlatform();
+
+        assertTrue(platform != "");
+    }
+}
---
+++ test-buddybuild-library/src/main/java/marcelosantosti/github/io/test_buddybuild_library/OsInfo.java
@@ -0,0 +1,33 @@
+package marcelosantosti.github.io.test_buddybuild_library;
+
+import android.os.Build;
+
+import java.lang.reflect.Field;
+
+/**
+ * Created by marcelosantos on 17/10/17.
+ */
+
+public class OsInfo {
+    public static String getPlatform() {
+        String osName = "";
+
+        try {
+            Field[] fields = Build.VERSION_CODES.class.getFields();
+            int position = Build.VERSION.SDK_INT + 1;
+
+            //if greater than KitKat, there is an additional version in 'fields' that is not actually used.
+            /*if (position > 20) {
+                position += 1;
+            }*/
+
+            osName = fields[position].getName();
+
+            return osName;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return osName;
+    }
+}
